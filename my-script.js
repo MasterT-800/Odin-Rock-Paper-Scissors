a6978aba35b17658b6aac3538efdee869699a7e0
tlinux <115206437+MasterT-800@users.noreply.github.com>
2024-01-10 18:47:07 -0500

    Add playRound function Function compares users choice against computers choice then returns the win or lose function unless both choices are equal. If equal it will call the playRound function again.

--- my-script.js
+++ my-script.js
@@ -8,3 +8,48 @@ function getComputerChoice() {
     let num = getRndInteger(0, 2);
     return choices[num];
 }
+
+function win(playerSelection, computerSelection) {
+    return 'You win! ' + playerSelection + ' beats' + computerSelection;
+}
+
+function lose(playerSelection, computerSelection) {
+    return 'You lose, ' + computerSelection + ' beats' + playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        playRound(prompt('Shoot', 'rock').toLowerCase(), getComputerChoice());
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return lose(playerSelection,computerSelection);
+        }
+        else {
+            return win(playerSelection, computerSelection);
+        }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return win(playerSelection, computerSelection);
+        }
+        else {
+            return lose(playerSelection,computerSelection);
+        }
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return win(playerSelection, computerSelection);
+        }
+        else {
+            return lose(playerSelection,computerSelection);
+        }
+    }
+    else { 
+        return 'Pleas choose either rock, paper, or scissors', playRound(prompt('Shoot', 'rock').toLowerCase(), getComputerChoice());
+    }
+    
+}
+
+const playerSelection = prompt('Shoot', 'rock').toLowerCase();
+const computerSelection = getComputerChoice();
